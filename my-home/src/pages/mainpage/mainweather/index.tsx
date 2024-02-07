ac1973c25732bafcd48e80817a71ea6ecc1b3c70
minyoung <minyoung99311@gmail.com>
2024-02-07 16:26:34 +0900

    :art: Mainpage 현재 날씨 api 연동
    mainpage 현재 날씨 api 연동

--- my-home/src/pages/mainpage/mainweather/index.tsx
+++ my-home/src/pages/mainpage/mainweather/index.tsx
@@ -1,14 +1,20 @@
+import { useWeatherGet } from '../../../hooks/weather/useWeatherGet';
 import { StyledMainWeather, StyledMainWeatherText } from './MainWeather.styled';
 
 const MainWeather = () => {
   // api 연동하고 수정
-  const recentweather = 'windy';
+  const { weatherCurrentInfo, weatherCurrentCityInfo, weatherCurrentMainInfo } = useWeatherGet();
+  const recentweather = { weatherCurrentMainInfo };
+
+  const today = new Date();
+  const formattedHours = today.getHours() < 10 ? `0${today.getHours()}` : today.getHours();
+  const recentTime = `${formattedHours}:00`;
 
   return (
     <StyledMainWeather recentweather={recentweather}>
-      <StyledMainWeatherText id="city">창원</StyledMainWeatherText>
-      <StyledMainWeatherText id="detail">경상남도 창원시 성산구, 12:00 기준</StyledMainWeatherText>
-      <StyledMainWeatherText id="temperature">10ºC</StyledMainWeatherText>
+      <StyledMainWeatherText id="city">{weatherCurrentCityInfo}</StyledMainWeatherText>
+      <StyledMainWeatherText id="detail">{recentTime} 기준</StyledMainWeatherText>
+      <StyledMainWeatherText id="temperature">{weatherCurrentInfo}ºC</StyledMainWeatherText>
     </StyledMainWeather>
   );
 };
